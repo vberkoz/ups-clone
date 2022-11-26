c3d2c96e6bbd9d2b26d4529c5c472b0efb507011
vberkoz <vberkoz@gmail.com>
2022-11-27 00:25:21 +0200

    Setting Up StepZen & Firebase (1/2)

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,14 +1,55 @@
+import tw from "twrnc";
+import React, { useLayoutEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  ActivityIndicator,
+} from "react-native";
 
-import tw from 'twrnc'
-import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { Input } from "@rneui/themed";
+
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const CustomersScreen = () => {
-  return (
-    <SafeAreaView>
-      <Text style={tw`text-blue-500`}>CustomersScreen</Text>
-    </SafeAreaView>
-  )
-}
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
+  const [input, setInput] = useState<string>("");
 
-export default CustomersScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <ScrollView style={{ backgroundColor: "#59c1cc" }}>
+      <Image
+        source={{ uri: "http://links.papareact.com/3jc" }}
+        style={tw`w-full h-64`}
+      />
+
+      <Input
+        placeholder="Search by Customer"
+        value={input}
+        onChangeText={setInput}
+        containerStyle={tw`bg-white pt-5 pb-0 px-10`}
+      />
+    </ScrollView>
+  );
+};
+
+export default CustomersScreen;
