15acfd90ea209c4b61fdf3124d0884d1294e798b
vberkoz <vberkoz@gmail.com>
2022-11-27 10:40:22 +0200

    Setting Up Apollo Client

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RootNavigator from "./navigator/RootNavigator";
 
-import RootNavigator from './navigator/RootNavigator';
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/nonplussed-dragonfly",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
