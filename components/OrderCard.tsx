f0e26e423f4288f1c7e1dfac5242182e9b4f62eb
vberkoz <vberkoz@gmail.com>
2022-11-29 11:58:14 +0200

    Building the Order Card Component

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,16 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import tw from "twrnc";
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { Card, Icon } from "@rneui/themed";
 
 type Props = {
-  item: Order
-}
+  item: Order;
+};
 
-const OrderCard = ({item}: Props) => {
+const OrderCard = ({ item }: Props) => {
   return (
-    <View>
-      <Text>OrderCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity>
+      <Card containerStyle={tw`px-5 rounded-lg`}>
+        <View style={tw`flex-row justify-between items-center`}>
+          <View>
+            <Icon
+              name="truck-delivery"
+              color={"#eb6a7c"}
+              type="material-community"
+            />
+            <Text style={{ fontSize: 10 }}>
+              {new Date(item.createdAt).toDateString()}
+            </Text>
+          </View>
 
-export default OrderCard
+          <View>
+            <Text style={[tw`text-gray-500`, { fontSize: 10 }]}>
+              {item.carrier}-{item.trackingId}
+            </Text>
+            <Text style={tw`text-gray-500 text-xl`}>
+              {item.trackingItems.customer.name}
+            </Text>
+          </View>
+
+          <View style={tw`flex-row items-center`}>
+            <Text style={[tw`text-sm`, { color: "#eb6a7c" }]}>
+              {item.trackingItems.items.length} x
+            </Text>
+            <Icon style={tw`ml-2`} name="box" type="feather" />
+          </View>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+export default OrderCard;
